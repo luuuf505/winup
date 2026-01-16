915f81ff5bb12f3f09a24026fbf3b2360dce69e8
luuuf505 <lufaarz@gmail.com>
2026-01-15 22:58:10 -0300

    Ajustes: homepage y base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/winup/"
-})
+  base: '/winup/'   // 👈 nombre exacto de tu repo en GitHub
+});
